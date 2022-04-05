85e0363582426d2d461e300658ae42198e0c9bbf
Peng Yao <yaopeng4504@gmail.com>
2022-04-05 14:49:16 +0300

    make the basic mqtt function work

--- main/app_main.c
+++ main/app_main.c
@@ -1,7 +1,9 @@
 #include <esp_http_server.h>
 #include <nvs_flash.h>
+#include <mqtt_client.h>
 #include "app_wifi.h"
 #include "app_http.h"
+#include "app_mqtt.h"
 
 void app_main()
 {
@@ -9,4 +11,9 @@ void app_main()
     wifi_init();
     httpd_handle_t server = NULL;
     server = webserver_start();
+    esp_mqtt_client_config_t mqtt_cfg = {
+        .uri = "mqtt://192.168.1.100",
+	.client_id = "esp32cam01"
+    };
+    mqtt_app_start(&mqtt_cfg);
 }
---
+++ main/include/app_mqtt.h
@@ -0,0 +1,4 @@
+#ifndef _APP_MQTT_H_
+#define _APP_MQTT_H_
+bool mqtt_app_start(esp_mqtt_client_config_t *mqtt_cfg);
+#endif
